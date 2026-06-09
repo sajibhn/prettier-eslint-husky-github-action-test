a4cd0b2624670f0d26967521f72f7fb7f6a7c87a
Sajib Hossain <sajibhossain1180@gmail.com>
2026-06-09 13:35:29 +0600

    feat: dashboard setup

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <Image
           className="dark:invert"
           src="/next.svg"
-          alt="Next.js logo"
+          alt="Next.js logos"
           width={100}
           height={20}
           priority
